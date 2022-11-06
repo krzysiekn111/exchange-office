dd466ea422fe5ef093b771913f3f50ebfdf5fcaa
krzysiekn111 <krzysiekn111@gmail.com>
2022-11-06 14:20:51 +0100

    wyszukiwanie działa

---
+++ src/main/java/pl/project/exchangeoffice/books/BookType.java
@@ -0,0 +1,5 @@
+package pl.project.exchangeoffice.books;
+
+public enum BookType {
+    IT, THRILLER, CLASSIC, POETRY
+}
--- src/main/java/pl/project/exchangeoffice/books/MainView.java
+++ src/main/java/pl/project/exchangeoffice/books/MainView.java
@@ -4,6 +4,8 @@ import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.data.value.ValueChangeMode;
 import com.vaadin.flow.router.Route;
 import pl.project.exchangeoffice.books.domain.Book;
 import pl.project.exchangeoffice.books.domain.BookService;
@@ -11,18 +13,30 @@ import pl.project.exchangeoffice.books.domain.BookService;
 @Route
 public class MainView extends VerticalLayout {
 
-    public void refresh() {
-        grid.setItems(bookService.getBooks());
-    }
+    private TextField filter = new TextField();
+    private BookService bookService = BookService.getInstance();
+    private Grid<Book> grid = new Grid<>(Book.class);
+
 
     public MainView() {
         grid.setColumns("title", "author", "publicationYear", "type");
         add(grid);
         setSizeFull();
         refresh();
+        filter.setPlaceholder("Filter by title");
+        filter.setClearButtonVisible(true);
+        filter.setValueChangeMode(ValueChangeMode.EAGER);
+        filter.addValueChangeListener(e -> update());
+        add(filter, grid);
     }
-    private BookService bookService = BookService.getInstance();
-    private Grid<Book> grid = new Grid<>(Book.class);
+
+    public void refresh() {
+        grid.setItems(bookService.getBooks());
+    }
+
+    private void update() {
+        grid.setItems(bookService.findByTitle(filter.getValue()));
+    }
 
 
 }
