a936bf1bbc7b105abf8c4fae965412891092267b
krzysiekn111 <krzysiekn111@gmail.com>
2022-11-28 19:51:22 +0100

    DZIAŁA!!!

--- src/main/java/pl/project/exchangeoffice/books/MainView.java
+++ src/main/java/pl/project/exchangeoffice/books/MainView.java
@@ -6,13 +6,18 @@ import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.data.value.ValueChangeMode;
 import com.vaadin.flow.router.Route;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.apache.commons.lang3.StringUtils;
 import pl.project.exchangeoffice.books.domain.Book;
 import pl.project.exchangeoffice.books.domain.BookService;
 import pl.project.exchangeoffice.books.domain.Rates;
-import pl.project.exchangeoffice.books.domain.Service;
 
 import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.List;
 
 @Route
 public class MainView extends VerticalLayout {
@@ -20,43 +25,66 @@ public class MainView extends VerticalLayout {
     private TextField filter = new TextField();
     private BookService bookService = BookService.getInstance();
     private Grid<Book> grid = new Grid<>(Book.class);
-//    private Grid<Rates> grid2 = new Grid<>(Rates.class);
-
-    @Autowired
-    private Service service;
+    private Grid<Rates> grid2 = new Grid<>(Rates.class);
 
 
     public MainView() throws IOException, InterruptedException {
-        Button button = new Button("Waluty");
-        button.addClickListener(e -> {
-            try {
-                Rates rates = service.getCurrencyValue("usd", 2);
-                System.out.println(rates);
-            } catch (Exception ex) {
-                ex.printStackTrace();
-            }
-        });
+        Button button = new Button(getCurrencyValue(2).toString());
         grid.setColumns("title", "author", "publicationYear", "type");
-//        grid2.setColumns("currency", "exchangeRateToPLN");
+        grid2.setColumns("currency", "exchangeRateToPLN");
         setSizeFull();
         refresh();
-//        refreshGrid2();
+        refreshGrid2();
         filter.setPlaceholder("Filter by title");
         filter.setClearButtonVisible(true);
         filter.setValueChangeMode(ValueChangeMode.EAGER);
         filter.addValueChangeListener(e -> update());
-        add(filter, grid, button);
-//        add(grid2);
+        add(filter, grid, button, grid2);
     }
 
     public void refresh() {
         grid.setItems(bookService.getBooks());
     }
-//    public void refreshGrid2() throws IOException, InterruptedException {
-//        grid2.setItems(service.getCurrencyValue("usd", 2));
-//    }
+    public void refreshGrid2() throws IOException, InterruptedException {
+        grid2.setItems(getCurrencies());
+    }
 
     private void update() {
         grid.setItems(bookService.findByTitle(filter.getValue()));
+    }
+
+    public String getConnection() throws IOException, InterruptedException {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("http://localhost:8090/v1/office/rates"))
+                .build();
+
+        return client.send(request, HttpResponse.BodyHandlers.ofString()).body();
+    }
+
+    public Rates getCurrencyValue(int id) throws IOException, InterruptedException {
+        String response = getConnection();
+        System.out.println(response + "response");
+        String[] s = response.split("\"currencyId\":" + id + ",\"exchangeRateToPLN\":");
+        String[] sc = s[1].split(",");
+        String[] scv = sc[1].split("\"currencyName\":\"");
+        String scvb = scv[1].split("\"")[0];
+
+        Rates rate = new Rates(
+                scvb,
+                Float.valueOf(sc[0])
+        );
+        return rate;
+    }
+    public List<Rates> getCurrencies() throws IOException, InterruptedException {
+        String response = getConnection();
+        int ids = StringUtils.countMatches(response, "id");
+        List<Rates> rates = new ArrayList<>();
+
+        for (int i = 0; i < ids; i++) {
+            rates.add(getCurrencyValue(i+1));
+        }
+
+        return rates;
     }
 }
--- src/main/java/pl/project/exchangeoffice/books/domain/Rates.java
+++ src/main/java/pl/project/exchangeoffice/books/domain/Rates.java
@@ -1,22 +1,50 @@
 package pl.project.exchangeoffice.books.domain;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import pl.project.exchangeoffice.books.MainView;
 
-@Getter
-//@AllArgsConstructor
-@NoArgsConstructor
+import java.util.Objects;
+
 public class Rates {
 
     private String currency;
     private Float exchangeRateToPLN;
+
+    public Rates() {}
 
     public Rates(String currency, Float exchangeRateToPLN) {
         this.currency = currency;
         this.exchangeRateToPLN = exchangeRateToPLN;
     }
 
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    public Float getExchangeRateToPLN() {
+        return exchangeRateToPLN;
+    }
+
+    public void setExchangeRateToPLN(Float exchangeRateToPLN) {
+        this.exchangeRateToPLN = exchangeRateToPLN;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Rates rates = (Rates) o;
+        return currency.equals(rates.currency) && exchangeRateToPLN.equals(rates.exchangeRateToPLN);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(currency, exchangeRateToPLN);
+    }
 
     @Override
     public String toString() {
@@ -25,4 +53,5 @@ public class Rates {
                 ", exchangeRateToPLN=" + exchangeRateToPLN +
                 '}';
     }
+
 }
